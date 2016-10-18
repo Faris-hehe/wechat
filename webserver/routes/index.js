9c5fac5513e408fad1494c76e876ed86fbf5c609
hyx <cuphyx@163.com>
2016-10-18 15:29:00 +0800

    'changeweixin-api'

--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -1,46 +1,57 @@
 var express = require('express');
+//var wechat = require('wechat');
+var weixin = require('weixin-api');
 //var weixin = require('wechat-api');
-var jsSHA = require('jssha');
+//var jsSHA = require('jssha');
 var router = express.Router();
 
+weixin.token = 'hellowechat';
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    var token = 'hellowechat';
-    var signature = req.query.signature;
-    var timestamp = req.query.timestamp;
-    var echostr = req.query.echostr;
-    var nonce = req.query.nonce;
+//router.get('/', function(req, res, next) {
+//    var token = 'hellowechat';
+//    var signature = req.query.signature;
+//    var timestamp = req.query.timestamp;
+//    var echostr = req.query.echostr;
+//    var nonce = req.query.nonce;
+//
+//    console.log(req.query);
+//    console.log( 'signature:'+signature );
+//    console.log( 'timestamp:'+timestamp );
+//    console.log( 'echostr:'+echostr );
+//    console.log( 'nonce:'+nonce );
+//
+//    var oriArray = [];
+//    oriArray[0] = token;
+//    oriArray[1] = timestamp;
+//    oriArray[2] = nonce;
+//    oriArray.sort();
+//
+//    var original = oriArray.join('');
+//    var shaObj = new jsSHA(original, 'TEXT');
+//    var scyptoString=shaObj.getHash('SHA-1', 'HEX');
+//
+//    if(signature == scyptoString){
+//        console.log('token succesed');
+//        res.status(200).send(echostr)
+//    } else {
+//        console.log('token failed');
+//        res.status(200).send('token验证失败')
+//    }
+//
+//});
+//
+//router.post('/', function(req, res, next) {
+//    res.send(req.query)
+//});
 
-    console.log(req.query);
-    console.log( 'signature:'+signature );
-    console.log( 'timestamp:'+timestamp );
-    console.log( 'echostr:'+echostr );
-    console.log( 'nonce:'+nonce );
-
-    var oriArray = [];
-    oriArray[0] = token;
-    oriArray[1] = timestamp;
-    oriArray[2] = nonce;
-    oriArray.sort();
-
-    var original = oriArray.join('');
-    var shaObj = new jsSHA(original, 'TEXT');
-    var scyptoString=shaObj.getHash('SHA-1', 'HEX');
-
-    if(signature == scyptoString){
-        console.log('token succesed');
-        res.status(200).send(echostr)
+router.get('/',function(req,res,next){
+    if (weixin.checkSignature(req)) {
+        console.log('token验证成功')；
+        res.status(200).send(req.query.echostr);
     } else {
-        console.log('token failed');
-        res.status(200).send('token验证失败')
+        res.status(200).send('fail');
     }
-
 });
-
-router.post('/', function(req, res, next) {
-    res.send(req.query)
-});
-
 
 
 module.exports = router;
